8e6601c5e7346dc994568cf1d59aaa76fa549bb0
Ken Mitton <mittonk@alum.mit.edu>
2024-09-21 00:15:50 -0400

    Moves through walls across entire playing field.

--- sokoban.asm
+++ sokoban.asm
@@ -66,6 +66,9 @@ ClearOam:
     ; Initialize global variables
     ld a, 0
     ld [wFrameCounter], a
+    ld [wCurKeys], a
+    ld [wNewKeys], a
+
 
 Main:
     ; Wait until it's *not* VBlank
@@ -77,6 +80,7 @@ WaitVBlank2:
     cp 144
     jp c, WaitVBlank2
 
+    ; Game clock
     ld a, [wFrameCounter]
     inc a
     ld [wFrameCounter], a
@@ -88,11 +92,108 @@ WaitVBlank2:
     ld [wFrameCounter], a
 
 
-    ; Move the player one pixel to the right.
+    ; Check the current keys every frame and move left or right.
+    call UpdateKeys
+
+    ; First, check if the left button is pressed.
+CheckLeft:
+    ld a, [wCurKeys]
+    and a, PADF_LEFT
+    jp z, CheckRight
+Left:
+    ; Move the paddle one square to the left.
     ld a, [_OAMRAM + 1]
-    inc a
+    sub a, 8
+    ; If we've already hit the edge of the playfield, don't move.
+    cp a, 8
+    jp z, Main
     ld [_OAMRAM + 1], a
     jp Main
+
+; Then check the right button.
+CheckRight:
+    ld a, [wCurKeys]
+    and a, PADF_RIGHT
+    jp z, CheckUp
+Right:
+    ; Move the paddle one square to the right.
+    ld a, [_OAMRAM + 1]
+    add a, 8
+    ; If we've already hit the edge of the playfield, don't move.
+    cp a, 160
+    jp z, Main
+    ld [_OAMRAM + 1], a
+    jp Main
+
+
+; Then check the up button.
+CheckUp:
+    ld a, [wCurKeys]
+    and a, PADF_UP
+    jp z, CheckDown
+Up:
+    ; Move the paddle one square up.
+    ld a, [_OAMRAM]
+    sub a, 8
+    ; If we've already hit the edge of the playfield, don't move.
+    cp a, 16
+    jp z, Main
+    ld [_OAMRAM], a
+    jp Main
+
+
+; Then check the down button.
+CheckDown:
+    ld a, [wCurKeys]
+    and a, PADF_DOWN
+    jp z, Main
+Down:
+    ; Move the paddle one square down.
+    ld a, [_OAMRAM]
+    add a, 8
+    ; If we've already hit the edge of the playfield, don't move.
+    cp a, 152
+    jp z, Main
+    ld [_OAMRAM], a
+    jp Main
+
+
+  ; https://gbdev.io/gb-asm-tutorial/part2/input.html
+UpdateKeys:
+  ; Poll half the controller
+  ld a, P1F_GET_BTN
+  call .onenibble
+  ld b, a ; B7-4 = 1; B3-0 = unpressed buttons
+
+  ; Poll the other half
+  ld a, P1F_GET_DPAD
+  call .onenibble
+  swap a ; A3-0 = unpressed directions; A7-4 = 1
+  xor a, b ; A = pressed buttons + directions
+  ld b, a ; B = pressed buttons + directions
+
+  ; And release the controller
+  ld a, P1F_GET_NONE
+  ldh [rP1], a
+
+  ; Combine with previous wCurKeys to make wNewKeys
+  ld a, [wCurKeys]
+  xor a, b ; A = keys that changed state
+  and a, b ; A = keys that changed to pressed
+  ld [wNewKeys], a
+  ld a, b
+  ld [wCurKeys], a
+  ret
+
+.onenibble
+  ldh [rP1], a ; switch the key matrix
+  call .knownret ; burn 10 cycles calling a known ret
+  ldh a, [rP1] ; ignore value while waiting for the key matrix to settle
+  ldh a, [rP1]
+  ldh a, [rP1] ; this read counts
+  or a, $F0 ; A7-4 = 1; A3-0 = unpressed keys
+.knownret
+  ret
 
 ; Copy bytes from one area to another.
 ; @param de: Source
@@ -269,4 +370,8 @@ PlayerEnd:
 
 SECTION "Counter", WRAM0
 wFrameCounter: db
+
+SECTION "Input Variables", WRAM0
+wCurKeys: db
+wNewKeys: db
 
